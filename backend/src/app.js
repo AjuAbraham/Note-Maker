16a913a3c4a0d4a3239af229d4764bc23ba9d8f0
Aju Abraham <ajubabraham23@gmail.com>
2024-02-14 12:18:05 +0530

    added express set

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,17 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import errorHandler from './middlewares/error.middleware.js';
 const app =express();
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
-    credentials:true,
+    credentials: true,
 }))
 app.use(cookieParser());
 app.use(express.json({limit:"16kb"}))
 
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static('public'));
-
+app.set("trust proxy",1);
 // routes
 import userRouter from './routes/user.routes.js'
 import noteRouter from './routes/note.routes.js';
